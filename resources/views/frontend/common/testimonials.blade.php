eb838572cdfaa597faf95b517aaf6dd0677ee192
adi080700 <adityacdingankar@gmail.com>
2021-10-23 20:37:54 +0530

    Initial commit

---
+++ resources/views/frontend/common/testimonials.blade.php
@@ -0,0 +1,28 @@
+  <div class="sidebar-widget  wow fadeInUp outer-top-vs ">
+              <div id="advertisement" class="advertisement">
+                <div class="item"> 
+                  <div class="avatar"><img src="{{ asset('frontend/assets/images/testimonials/aditya.jpg') }}" alt="Image"></div>
+                  <div class="testimonials"><em>"</em>"We want to create a website which will cater to all your fashion needs!"<em>"</em></div>
+                  <div class="clients_author">Adiya Dingankar <span>IT Student</span> </div>
+                  <!-- /.container-fluid --> 
+                </div>
+                <!-- /.item -->
+                
+                <div class="item">
+                  <div class="avatar"><img src="{{ asset('frontend/assets/images/testimonials/Vedant.png') }}" alt="Image"></div>
+                  <div class="testimonials"><em>"</em>XYZ.<em>"</em></div>
+                  <div class="clients_author">Vedant Jadia<span>IT Student</span> </div>
+                </div>
+                <!-- /.item -->
+                
+                <div class="item">
+                  <div class="avatar"><img src="{{ asset('frontend/assets/images/testimonials/karan.png') }}" alt="Image"></div>
+                  <div class="testimonials"><em>"</em> ABC<em>"</em></div>
+                  <div class="clients_author">Karan Jain<span>IT Student</span> </div> 
+                 
+                </div>
+                <!-- /.item --> 
+                
+              </div>
+              <!-- /.owl-carousel --> 
+            </div>
